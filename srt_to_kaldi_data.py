b653a4d62fe881f8cd1cdbceb29d1a5c079bd89f
Andrea Carmantini <stic.lorenzo@gmail.com>
2018-09-06 13:23:29 +0100

    Added audio conversion pipe, sorted output, checks for timings and text

--- srt_to_kaldi_data.py
+++ srt_to_kaldi_data.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
 import codecs
@@ -14,8 +15,8 @@ exclude_punct = re.compile(ur'[^\P{P}\']+')  # everything but '
 class utterance(object):
     def __init__(self, utt_id, start, end, text):
         self.utt_id = utt_id
-        self.start = float(start)/1000.0
-        self.end = float(end)/1000.0
+        self.start = start/1000.0
+        self.end = end/1000.0
         self.text = exclude_punct.sub('', text.lower())
 
 
@@ -26,9 +27,15 @@ class recording(object):
         self.utt_number = 0
 
     def add_utterance(self, start, end, text):
-        if text.strip():
+        if text.strip() and start < end:
             self.utterances.append(utterance(self.utt_number, start, end, text))
             self.utt_number += 1
+
+    def __lt__(self, other):
+        return self.basename < other.basename
+
+    def __len__(self):
+        return len(self.utterances)
 
 
 def parse_srt(filename):
@@ -48,6 +55,8 @@ def main(srt_dir, audio_dir, out_dir, threads=10):
     thr_pool = Pool(threads)
     recordings = (thr_pool.map(parse_srt, glob.glob(srt_dir + '/*.srt')))
 
+    recordings = [x for x in recordings if len(x) > 0]  # Recordings must have at least one segment
+
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
@@ -55,9 +64,12 @@ def main(srt_dir, audio_dir, out_dir, threads=10):
     wav = codecs.open(out_dir + '/wav.scp', 'w', 'utf-8')
     utt2spk = codecs.open(out_dir + '/utt2spk', 'w', 'utf-8')
     txt = codecs.open(out_dir + '/text', 'w', 'utf-8')
+
     try:
-        for reco in recordings:
-            wav.write('{} ffmpeg {}/{}.aac\n'.format(reco.basename, audio_dir, reco.basename))
+        for reco in sorted(recordings):
+            wav.write('{} ffmpeg -loglevel panic -i {}/{}.aac -ac 1 -ar 16000 -f wav - |\n'.format(
+                reco.basename, os.path.abspath(audio_dir), reco.basename))
+
             for utt in reco.utterances:
                 txt.write(u'{}-{:04d} {}\n'.format(reco.basename, utt.utt_id, utt.text))
 
@@ -72,6 +84,7 @@ def main(srt_dir, audio_dir, out_dir, threads=10):
         wav.close()
         utt2spk.close()
         txt.close()
+
 
 if __name__ == "__main__":
     srt_dir = sys.argv[1]
